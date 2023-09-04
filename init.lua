a1f176e0f6afe58eadc4649e2e93491a5622a085
suhrm <suhrminator@gmail.com>
2023-09-04 10:12:54 +0200

    update

--- init.lua
+++ init.lua
@@ -656,8 +656,8 @@ require('toggleterm').setup {
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 -- Setup harpoon
-vim.keymap.set('n', "<C-e>",mark.add_file)
-vim.keymap.set('n', "<leader>e", ui.toggle_quick_menu)
+vim.keymap.set('n', "<C-s>",mark.add_file)
+vim.keymap.set('n', "<C-e>", ui.toggle_quick_menu)
 vim.keymap.set('n', "<leader>1", function() ui.nav_file(1) end)
 vim.keymap.set('n', "<leader>2", function() ui.nav_file(2) end)
 vim.keymap.set('n', "<leader>3", function() ui.nav_file(3) end)
